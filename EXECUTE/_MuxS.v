fc6fd32427a4e90eb74ea50ad0fece94c7f2b94e
MauriSinM <142054167+MauriSinM@users.noreply.github.com>
2025-11-30 17:55:51 -0600

    Update _MuxS.v

--- EXECUTE/_MuxS.v
+++ EXECUTE/_MuxS.v
@@ -1,8 +1,8 @@
 module _MuxS (
     input wire sel,
-    input wire [4:0] A,
-    input wire [4:0] B,
-    output wire [4:0] s
+    input wire [31:0] A,
+    input wire [31:0] B,
+    output wire [31:0] s
 );
     assign s = (sel) ? B : A;
 endmodule
