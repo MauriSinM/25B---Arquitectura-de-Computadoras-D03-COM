15789cfaca8579f7c7b5f92c2b457d9751df86e3
MauriSinM <142054167+MauriSinM@users.noreply.github.com>
2025-11-30 16:59:28 -0600

    Update _CF-DE.v

--- FETCH/_CF-DE.v
+++ FETCH/_CF-DE.v
@@ -1,12 +1,22 @@
-module _cf_de (
+module _CF_DE (
     input wire clk,
     input wire [31:0] Inst,       // instrucción de entrada
-    output reg [31:0] Bytes       // instrucción almacenada
+    input wire [31:0] PC,         // contador de programa
+    input wire [31:0] PC_plus_4,  // PC + 4
+    output reg [31:0] Bytes,      // instrucción almacenada
+    output reg [31:0] PC_out,     // PC almacenado
+    output reg [31:0] PC_plus_4_out // PC + 4 almacenado
 );
-    initial Bytes = 32'b0;
+    initial begin
+        Bytes = 32'b0;
+        PC_out = 32'b0;
+        PC_plus_4_out = 32'b0;
+    end
 
     always @(posedge clk) begin
         Bytes = Inst;
+        PC_out = PC;
+        PC_plus_4_out = PC_plus_4;
     end
 
 endmodule
