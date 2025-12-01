42f0da2eae48542d843c35c78e251102d3c09a51
MauriSinM <142054167+MauriSinM@users.noreply.github.com>
2025-11-30 18:26:13 -0600

    Enhance _pc module to manage Dir states
    Add handling for unknown or high-impedance states in Dir.

--- FETCH/_pc.v
+++ FETCH/_pc.v
@@ -1,12 +1,15 @@
 module _pc(
-	input clk,
-	input [31:0] DirSig,
-	output reg [31:0] Dir
+    input clk,
+    input [31:0] DirSig,
+    output reg [31:0] Dir
 );
-	initial Dir = 0;
-	
-	always @(posedge clk) begin
-		Dir = DirSig;
-	end
-
+    initial Dir = 32'b0;
+    
+    always @(posedge clk) begin
+        if (Dir === 32'bx || Dir === 32'bz) begin  // Si es desconocido o alta impedancia
+            Dir <= 32'b0;
+        end else begin
+            Dir <= DirSig;
+        end
+    end
 endmodule
